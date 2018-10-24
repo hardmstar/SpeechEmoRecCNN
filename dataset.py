1d91abf95ec34c3c13f3f85f0904b54fd3fc4c50
hardmstar <hardmstar@126.com>
2018-10-24 15:28:57 +0800

    modify dataset structure, features cut into 300 frames a block

--- dataset.py
+++ dataset.py
@@ -10,7 +10,9 @@ class Dataset:
     def __init__(self, type):
         self.train_path = []
         self.val_path = []
-        if type == 'berlin':
+        self.type = type
+
+        if self.type == 'berlin':
             self.root = 'EMODB/'
             self.speakers = ['03', '08', '09', '10', '11', '12', '13', '14', '15', '16']
             self.classes = {0: 'W', 1: 'L', 2: 'E', 3: 'A', 4: 'F', 5: 'T', 6: 'N'}
@@ -18,42 +20,46 @@ class Dataset:
             # python 3.x use items() , 2.x use iteritems()
             self.wav_files = self.root + 'wav'
             self.NN_inputs = self.root + 'NN_inputs/'
-            self.get_dataset('berlin')
+            # self.get_dataset()
 
-    def get_dataset(self, type):
-        for speaker in self.speakers:
-            speakerhome = self.root + speaker + '/'
-            if not os.path.exists(speakerhome):
-                os.makedirs(speakerhome)
 
-            self.train_path.append(speakerhome + 'train.txt')
-            self.train_path.append(speakerhome + 'val.txt')
+def get_dataset(self):
+    for speaker in self.speakers:
+        speakerhome = self.root + speaker + '/'
+        if not os.path.exists(speakerhome):
+            os.makedirs(speakerhome)
 
-            # speaker independent training and validation
-            # train files describe wav files and its class
-            # validation files the same as train files
-            train_file = open(speakerhome + 'train.txt', 'a')
-            val_file = open(speakerhome + 'val.txt','a')
+        self.train_path.append(speakerhome + 'train.txt')
+        self.train_path.append(speakerhome + 'val.txt')
 
-            for wav in os.listdir(self.wav_files):
-                # generate lists of train files and validation files
-                if self.return_speaker(type, wav) == speaker:
-                    val_file.write('%s %s\n' %(self.NN_inputs + wav, self.return_class(type, wav)))
-                else:
-                    train_file.write('%s %s\n' %(self.NN_inputs + wav, self.return_class(type, wav)))
+        # speaker independent training and validation
+        # train files describe wav files and its class
+        # validation files the same as train files
+        train_file = open(speakerhome + 'train.txt', 'a')
+        val_file = open(speakerhome + 'val.txt', 'a')
 
-            train_file.close()
-            val_file.close()
+        for wav in os.listdir(self.wav_files):
+            # generate lists of train files and validation files
+            if return_speaker(self.type, wav) == speaker:
+                val_file.write('%s %s\n' % (self.NN_inputs + wav, return_class(self.type, wav)))
+            else:
+                train_file.write('%s %s\n' % (self.NN_inputs + wav, return_class(self.type, wav)))
 
-    def return_speaker(self, type, wav):
-        # return speaker of the wav file
-        if type == 'berlin':
-            return wav[:2]
+        train_file.close()
+        val_file.close()
 
-    def return_class(self, type, wav):
-        # return emotion of this wav file
-        if type == 'berlin':
-            return self.classes_num[wav[5]]
+
+def return_speaker( type, wav):
+    # return speaker of the wav file
+    if type == 'berlin':
+        return wav[:2]
+
+
+def return_class( type, wav):
+    # return emotion of this wav file
+    if type == 'berlin':
+        exp = Dataset('berlin')
+        return exp.classes_num[wav[5]]
 
 
 dataset = Dataset('berlin')
