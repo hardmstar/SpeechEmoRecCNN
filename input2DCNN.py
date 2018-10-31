5b82fec6adc70e3a7e4eaa8cb656c986415207e8
hardmstar <hardmstar@126.com>
2018-10-31 18:11:21 +0800

    how to load model

--- input2DCNN.py
+++ input2DCNN.py
@@ -8,7 +8,7 @@ import os
 
 
 class wavDataGenerator:
-    def __init___(self, txt_file, batch_size, num_class, shuffle=True, buffer_size=1000):
+    def __init__(self, txt_file, batch_size, num_class, shuffle=True, buffer_size=1000):
         '''
         args:
             txt_file: txt file contains feature file paths
@@ -48,11 +48,30 @@ class wavDataGenerator:
 
         # convert numpy array into tf tensor
         np_features = tf.convert_to_tensor(np.array(np_features))
+        np_features = tf.reshape(np_features,[-1,300,23,1])
         labels = tf.convert_to_tensor(labels)
         self.data = tf.data.Dataset.from_tensor_slices((np_features, labels))
 
 
-def train_session(ob_dataset, speaker, checkpoints_path, batch_size):
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.7)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(a, w):
+    return tf.nn.conv2d(a, w, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(a):
+    return tf.nn.max_pool(a, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+def train_session(ob_dataset , speaker, checkpoints_path, batch_size):
     '''
     train session of ser system
     '''
@@ -60,15 +79,15 @@ def train_session(ob_dataset, speaker, checkpoints_path, batch_size):
     # create checkpoint path
     if not os.path.isdir(checkpoints_path):
         os.makedirs(checkpoints_path)
-    num_speakers = len(ob_dataset.speakers)
-    train_txt_file = ob_dataset.root + speaker + '/train_segments.txt'
+    num_classes = len(ob_dataset.classes)
     # loading and preprocessing train and validation data on the cpu
     with tf.device('/cpu:0'):
         # def __init___(self, txt_file, batch_size, num_class, shuffle=True, buffer_size=1000):
-        tr_data = wavDataGenerator(train_txt_file, batch_size, num_speakers)
+        # tr_data = wavDataGenerator(train_txt_file, batch_size, num_speakers)
+        tr_data = wavDataGenerator(ob_dataset.root + speaker + '/train_segments.txt' , batch_size, num_classes)
         val_data = wavDataGenerator(ob_dataset.root + speaker + '/val_segments.txt',
                                     batch_size=batch_size,
-                                    num_class=len(ob_dataset.speakers))
+                                    num_class=num_classes)
 
         # create an reuseble initializable iterator
         iterator = tf.data.Iterator.from_structure(tr_data.data.output_types,
@@ -80,25 +99,27 @@ def train_session(ob_dataset, speaker, checkpoints_path, batch_size):
     val_init_op = iterator.make_initializer(val_data.data)
 
     # https://blog.csdn.net/sinat_33761963/article/details/57416517
-    y_ = tf.placeholder("float", [None, len(ob_dataset.speakers)])
+    x = tf.placeholder("float32",[None,300,23,1], name='x')
+    y_ = tf.placeholder("float", [None, len(ob_dataset.classes)],name='y')
+    keep_prob = tf.placeholder(tf.float32)
     # convolution layer and pool layer
     w_conv1 = weight_variable([5, 5, 1, 32])  # ? ? ? why 5
     b_conv1 = bias_variable([32])
     # conv output : 300 * 23 * 32
-    h_conv1 = tf.nn.relu(conv2d(tr_data.data.output_shapes, w_conv1) + b_conv1)
+    h_conv1 = tf.nn.relu(conv2d(x, w_conv1) + b_conv1)
     # pool output : 250 * 17 * 32
     h_pool1 = max_pool_2x2(h_conv1)
 
     # full connection layer
-    w_fc1 = weight_variable([250 * 17 * 32, 1024])
+    w_fc1 = weight_variable([150*12 * 32, 1024])
     b_fc1 = bias_variable([1024])
     # 数据拉平为一行
-    h_pool1_flat = tf.reshape(h_pool1, [-1, 250 * 17 * 32])
+    h_pool1_flat = tf.reshape(h_pool1, [-1, 150*12 * 32])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool1_flat, w_fc1) + b_fc1)
-    h_fc1_drop = tf.nn.dropout(h_fc1, dropout_rate)
+    h_fc1_drop = tf.nn.dropout(h_fc1, 0.5)
     # fc 2
-    w_fc2 = weight_variable([1024, len(ob_dataset.speakers)])
-    b_fc2 = bias_variable([len(ob_dataset.speakers)])
+    w_fc2 = weight_variable([1024, num_classes])
+    b_fc2 = bias_variable([num_classes])
     y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
 
     # optimization
@@ -107,7 +128,7 @@ def train_session(ob_dataset, speaker, checkpoints_path, batch_size):
 
     # calculate accuracy
     correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"),name='accuracy')
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -117,39 +138,47 @@ def train_session(ob_dataset, speaker, checkpoints_path, batch_size):
                 data_batch, label_batch = sess.run(next_batch)
                 train_accuracy = accuracy.eval(feed_dict={x: data_batch, y_: label_batch, keep_prob: 1.0})
                 print("step %d, train_accuracy %g" % (step, train_accuracy))
-                train_step.run(feed_dict: {x: data_batch, y_: label_batch, keep_prob: 0.5})
+                train_step.run( feed_dict={x: data_batch, y_: label_batch, keep_prob: 0.5})
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.7)
-    return tf.Variable(initial)
+        # save check points , which include weights, bias etc
+        checkpoint_name = checkpoints_path+ 'model_epoch'+str(epoch)+'.ckpt'
+        saver = tf.train.Saver()
+        save_path = saver.save(sess, checkpoint_name)
 
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+    checkpoint_file = tf.train.latest_checkpoint(checkpoint_name)
+    with tf.Session() as sess:
+        new_saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+        new_saver.restore(sess, checkpoint_file)
+        graph = tf.get_default_graph()
+        x = graph.get_operation_by_name('x').outputs[0]
+        y_ = graph.get_operation_by_name('y_').outputs[0]
+        acc = graph.get_operation_by_name('accuracy').outputs[0]
+        sess.run(val_init_op)
+        for step in range(2):
+            data_batch, label_batch = sess.run(next_batch)
+            sess.run(acc,feed_dict={x:data_batch,y_:label_batch})
+            print(acc)
 
-def conv2d(a, w):
-    return tf.nn.conv2d(a, w, strides=[1, 1, 1, 1], padding='SAME')
 
-def max_pool_2x2(a):
-    return tf.nn.max_pool(a, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    if __name__ == '__main__':
-        berlin = Dataset('berlin')
-        # if not exists train and validation file,
-        # berlin.get_dataset('berlin')
 
-        # learning params
-        learning_rate = 1e-3
-        num_epochs = 300
-        batch_size = 30
-        # Network params
-        dropout_rate = 0.5
+if __name__ == '__main__':
+    berlin = Dataset('berlin')
+    # if not exists train and validation file,
+    # berlin.get_dataset('berlin')
 
-        # Path for tf.summary.FileWriter and to store model checkpoints
-        filewriter_path = berlin.root + '/tmp/tensorboard'
-        checkpoints_path = berlin.root + '/tmp/checkpoints'
+    # learning params
+    learning_rate = 1e-3
+    num_epochs = 300
+    batch_size = 30
+    # Network params
+    dropout_rate = 0.5
 
-        # x = tf.placeholder(tf.float32, [None, 300,], name='input')
-        # y = tf.placeholder(tf.float32, [None, len(berlin.classes]))
-        for speaker in berlin.speakers:
-            train_session(berlin, speaker,checkpoints_path,batch_size)
+    # Path for tf.summary.FileWriter and to store model checkpoints
+    filewriter_path = berlin.root + '/tmp/tensorboard/'
+    checkpoints_path = berlin.root + '/tmp/checkpoints/'
+
+    # x = tf.placeholder(tf.float32, [None, 300,], name='input')
+    # y = tf.placeholder(tf.float32, [None, len(berlin.classes]))
+    for speaker in berlin.speakers:
+        train_session(berlin, speaker,checkpoints_path,batch_size)
