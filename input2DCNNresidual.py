2cb3b6c9a004f4e93e35078e7b094e60121aec1f
hardmstar <hardmstar@126.com>
2018-11-05 21:10:12 +0800

    2d residual cnn can train and validation now

--- input2DCNNresidual.py
+++ input2DCNNresidual.py
@@ -6,32 +6,38 @@ import numpy as np
 import os
 from wavDataGenerator import *
 
+learning_rate = 1e-3
+
 
 def residual_block(x, num_input_filters, num_output_filters, block_num):
-    # define weights and biases of this residual block
-    w_conv_1 = tf.get_variable(name='rs_block_' + str(block_num) + '_w_conv_1',
-                               shape=[3, 3, num_input_filters, num_output_filters], dtype=tf.float32)
-    w_conv_2 = tf.get_variable(name='rs_block_' + str(block_num) + '_w_conv_2',
-                               shape=[3, 3, num_input_filters, num_output_filters], dtype=tf.float32)
-    b_conv_1 = tf.get_variable(name='rs_block_' + str(block_num) + '_b_conv_1', shape=[num_output_filters],
-                               dtype=tf.float32)
-    b_conv_2 = tf.get_variable(name='rs_block_' + str(block_num) + '_b_conv_2', shape=[num_output_filters],
-                               dtype=tf.float32)
-
-    # implementing residual block logic
-    input_1 = tf.contrib.layers.batch_norm(x)
-    input_1 = tf.nn.relu(input_1)
-    weight_layer_1 = tf.nn.conv2d(input_1, w_conv_1, strides=[1, 1, 1, 1], padding='SAME') + b_conv_1
-    intermediate = tf.contrib.layers.batch_norm(weight_layer_1)
-    num_input_filters = tf.nn.relu(intermediate)
-    weight_layer_2 = tf.nn.conv2d(intermediate, w_conv_2, strides=[1, 1, 1, 1], padding='SAME') + b_conv_2
-
-    # elementwise addition of x and weight_layer_2
-    if num_input_filters != num_output_filters:
-        w_conv_increase = tf.get_variable('rs_block_' + str(block_num) + '_w_conv_increase',
-                                          shape=[1, 1, num_input_filters], dtype=tf.float32)
-        b_conv_increase = tf.get_variable('rs_block_' + str(block_num) + '_b_conv_increase', shape=[num_output_filters],
-                                          dtype=tf.float32)
+    with tf.variable_scope('residual_block_'+str(block_num)):
+        # define weights and biases of this residual block
+        w_conv_1 = tf.get_variable(name='rs_block_' + str(block_num) + '_w_conv_1',
+                                   shape=[3, 3, num_input_filters, num_output_filters], dtype=tf.float32)
+        w_conv_2 = tf.get_variable(name='rs_block_' + str(block_num) + '_w_conv_2',
+                                   shape=[3, 3, num_output_filters, num_output_filters], dtype=tf.float32)
+        b_conv_1 = tf.get_variable(name='rs_block_' + str(block_num) + '_b_conv_1', shape=[num_output_filters],
+                                   dtype=tf.float32)
+        b_conv_2 = tf.get_variable(name='rs_block_' + str(block_num) + '_b_conv_2', shape=[num_output_filters],
+                                   dtype=tf.float32)
+    
+        # implementing residual block logic
+        input_1 = tf.contrib.layers.batch_norm(x)
+        input_1 = tf.nn.relu(input_1)
+        weight_layer_1 = tf.nn.conv2d(input_1, w_conv_1, strides=[1, 1, 1, 1], padding='SAME') + b_conv_1
+        intermediate = tf.contrib.layers.batch_norm(weight_layer_1)
+        intermediate = tf.nn.relu(intermediate)
+        weight_layer_2 = tf.nn.conv2d(intermediate, w_conv_2, strides=[1, 1, 1, 1], padding='SAME') + b_conv_2
+    
+        # elementwise addition of x and weight_layer_2
+        if num_input_filters != num_output_filters:
+            w_conv_increase = tf.get_variable('rs_block_' + str(block_num) + '_w_conv_increase',
+                                              shape=[1, 1, num_input_filters,num_output_filters],
+                                              dtype=tf.float32)
+            b_conv_increase = tf.get_variable('rs_block_' + str(block_num) + '_b_conv_increase',
+                                              shape=[num_output_filters],
+                                              dtype=tf.float32)
+            x = tf.nn.conv2d(x, w_conv_increase, strides=[1,1,1,1], padding='SAME') + b_conv_increase
     output = tf.add(x, weight_layer_2)
     return output
 
@@ -53,17 +59,18 @@ def recurrent_neural_network(x, NUM_CLASSES):
     rs_block_11 = residual_block(rs_block_10, 256, 256, 11)
     rs_block_12 = tf.nn.max_pool(rs_block_11, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    num_features = rs_block_12.shape[1] * rs_block_12.shape[2]
+    num_features = rs_block_12.shape[1] * rs_block_12.shape[2] * rs_block_12.shape[3]
     flattened = tf.reshape(rs_block_12, [-1, num_features])
 
-    # fully connected layers
-    w_fc1 = tf.get_variable('w_fc1', shape=[num_features, 128], dtype=tf.float32)
-    w_fc2 = tf.get_variable('w_fc2', shape=[128, NUM_CLASSES], dtype=tf.float32)
-    b_fc1 = tf.get_variable('b_fc1', shape=[128], dtype=tf.float32)
-    b_fc2 = tf.get_variable('b_fc2', shape=[NUM_CLASSES], dtype=tf.float32)
-
-    fully_connected_1 = tf.matmul(flattened, w_fc1) + b_fc1
-    fully_connected_2 = tf.matmul(fully_connected_1, w_fc2) + b_fc2
+    with tf.variable_scope('recurrent_neural_network_full_connection'):
+        # fully connected layers
+        w_fc1 = tf.get_variable('w_fc1', shape=[num_features, 128], dtype=tf.float32)
+        w_fc2 = tf.get_variable('w_fc2', shape=[128, NUM_CLASSES], dtype=tf.float32)
+        b_fc1 = tf.get_variable('b_fc1', shape=[128], dtype=tf.float32)
+        b_fc2 = tf.get_variable('b_fc2', shape=[NUM_CLASSES], dtype=tf.float32)
+    
+        fully_connected_1 = tf.matmul(flattened, w_fc1) + b_fc1
+        fully_connected_2 = tf.matmul(fully_connected_1, w_fc2) + b_fc2
 
     return fully_connected_2
 
@@ -90,58 +97,85 @@ def train_seesion(ob_dataset, speaker, checkpoints_path, num_epochs, batch_size)
     val_init_op = iterator.make_initializer(val_data.data)
 
     ########## test iterator
+    '''
     sess = tf.Session()
     for _ in range(20):
         sess.run(train_init_op)
-        for _ in range(100):
+        for _ in range(1):
             print(sess.run(next_batch))
 
         sess.run(val_init_op)
-        for _ in range(100):
+        for _ in range(1):
             print(sess.run(next_batch))
+    '''
     ##########
-    x = tf.placeholder('float32',[None, 300, 23, 1], name='input_x')
+    x = tf.placeholder('float32', [None, 300, 23, 1], name='input_x')
     y = tf.placeholder('float32', [None, num_classes], name='label_y')
 
-    logits = recurrent_neural_network(x,num_classes)
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
-    optimizer = tf.train.AdamOptimizer()
-    training = optimizer.minimize(loss)
+    logits = recurrent_neural_network(x, num_classes)
+    y_prediction = tf.nn.softmax(logits)
+    # loss function
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_prediction, labels=y))
+    # optimizer
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 
+
+    train_op = optimizer.minimize(loss)
+    # accuracy
+    with tf.name_scope('accuracy'):
+        correct_pred = tf.equal(tf.argmax(y_prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    tf.summary.scalar('accuracy', accuracy)
+    
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-
+    
         # for epoch in range(num_epochs)
         for epoch in range(2):
             total_cost = 0
-
-            # for i in range(NUM_EXAMPLES/BATCH_SIZE):
+    
+            # train start
+            print('train start')
+            sess.run(train_init_op)
+            train_acc = 0
+            train_count = 0
+            train_loss = 0
+            # for i in range(tr_data.data_size // batch_size):
             for i in range(2):
                 batch_x, batch_y = sess.run(next_batch)
-                _, batch_cost = sess.run([training, loss], feed_dict={x: batch_x, y: batch_y})
-                total_cost += batch_cost
-                if i % 25 == 0:
-                    print(i)
-            print('Epoch ', epoch, ' Cost: total_cost')
+                # logits_return, y_prediction_return = sess.run((logits, y_prediction),feed_dict={x: batch_x, y: batch_y})
+                train_op_return, train_acc_value, train_loss_value = sess.run((train_op, accuracy, loss),
+                                                                              feed_dict={x: batch_x, y: batch_y})
+                train_loss += train_loss_value
+                train_acc = train_acc_value
+                train_count += 1
+            train_loss /= train_count
+            train_acc /= train_count
+            print('speaker {} in epoch {}, train_acc={}, train_loss={}'.format(speaker, epoch, train_acc, train_loss))
 
-            # predict validation accuray after every epoch
-            sum_accuracy_validation = 0.0
-            sum_i = 0
-            # for i in range(int(dataset_validation_features.shape[0]/BATCH_SIZE))
-            batch_x, batch_y = sess.run(next_batch)
-            y_predicted = tf.nn.softmax(logits)
-            correct = tf.equal(tf.argmax(y_predicted, 1), tf.argmax(y, 1))
-            accuracy_function = tf.reduce_mean(tf.cast(correct, 'float'))
-            accuracy_validation = accuracy_function.eval({x: batch_x, y: batch_y})
-            sum_accuracy_validation += accuracy_validation
-            sum_i += 1
-            print('validation accuracy in epoch ', epoch, ': ', accuracy_validation, 'sum_i: ', sum_i,
-                  'sum_accuracy_validation', sum_accuracy_validation)
 
+            scope_now = tf.get_variable_scope()
+            scope_now.reuse_variables()
+            # validation start
+            print('start validation')
+            sess.run(val_init_op)
+            test_acc = 0
+            test_count = 0
+            test_loss = 0
+            # for _ in range(val_data.data_size // batch_size):
+            for _ in range(2):
+                batch_x, batch_y = sess.run(next_batch)
+                acc, loss_value = sess.run((accuracy, loss), feed_dict={x: batch_x, y: batch_y})
+                test_loss += loss_value
+                test_acc += acc
+                test_count += 1
+            test_acc /= test_count
+            test_loss /= test_count
+            print('speaker {} in epoch {}, test_acc={}, test_loss={}'.format(speaker, epoch, test_acc, test_loss))
+            
 if __name__ == '__main__':
     berlin = Dataset('berlin')
 
-    learning_rate = 1e-3
     num_epochs = 300
     batch_size = 30
     dropout_rate = 0.5
